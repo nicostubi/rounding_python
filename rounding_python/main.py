bc71c6835af749fd5adf9d90f5d51d481d61fcf0
Nicolas Stübi <nicolas.stubi@romande-energie.ch>
2024-06-27 19:25:02 +0200

    fix: some more improvements in formatting and fixed nb of iterations to ensure test exactness

--- rounding_python/main.py
+++ rounding_python/main.py
@@ -7,11 +7,18 @@ def print_result(method, result, expected):
         failed = '\u274c'
         print(f'{method} => {result} {failed} (expected {expected})')
         return False
-    print(f'{method} => {result} {passed}')
+    print(f'{method} => {result:.4f} {passed}')
     return True
 
 
 # def round_to_four_decimals(value):
+#     # Multiplie la valeur par 10000, la transforme en entier et la divise par 10000 pour garder 4 décimales
+#     temp_value = int(value * 10000) / 10000.0
+#     # Formatte la valeur pour s'assurer qu'elle a toujours 4 décimales
+#     formatted_value = f"{temp_value:.4f}"
+#     return float(formatted_value)
+
+# def round_to_four_decimals_with_decimal(value):
 #     # Multiplie la valeur par 10000, la transforme en entier et la divise par 10000 pour garder 4 décimales
 #     temp_value = int(value * 10000) / 10000.0
 #     # Formatte la valeur pour s'assurer qu'elle a toujours 4 décimales
@@ -48,9 +55,9 @@ def execute_value_chf_and_decimals(value_chf, expected, decimals, n):
     print_result('rounded_to_4_decimal', rounded_to_4_decimal, expected)
 
 
-def execute_round_to_four_decimals_with_decimal(value_chf, expected, decimals, n):
+def execute_round_to_four_decimals_with_decimal(value_chf, expected, decimals, n, format_str):
     print('---------------------------------------------------')
-    print(f'CAS DE TEST {n} | montant_chf: {value_chf} | decimals: {decimals} | expected: {expected}')
+    print(f'CAS DE TEST {n} | montant_chf: {format_str.format(value_chf)} | decimals: {decimals} | expected: {expected:.4f}')
     print('---------------------------------------------------')
     rounded_to_4_decimal = round_to_four_decimals_with_decimal(value_chf)
     return print_result('rounded_to_4_decimal', rounded_to_4_decimal, expected)
@@ -71,23 +78,24 @@ def execute_round_to_four_decimals_with_decimal(value_chf, expected, decimals, n
 # print('---TEST WITH ALL VALUES BETWEEN 0 and 1 6 DEC------')
 # print('---------------------------------------------------')
 
-val = 0.000001
-increment = 0.000001
-nb_failed_tests = 0
-test_case_id = 1
-iterations = 1000000
-
-for _ in range(iterations):
-    result = execute_round_to_four_decimals_with_decimal(val, float(str(val)[0:6]), 4, test_case_id)
-    if not result:
-        nb_failed_tests += 1
-
-    val += increment
-    test_case_id += 1
-
-
-print(f'Nb of tests done: {test_case_id-1}')
-print(f'Nb of tests failed: {nb_failed_tests}')
+# val = 0.000001
+# increment = 0.000001
+# nb_failed_tests = 0
+# test_case_id = 1
+# iterations = 999999
+# format_str = "{:.6f}"
+#
+# for _ in range(iterations):
+#     result = execute_round_to_four_decimals_with_decimal(val, float(str(val)[0:6]), 4, test_case_id, format_str)
+#     if not result:
+#         nb_failed_tests += 1
+#
+#     val += increment
+#     test_case_id += 1
+#
+#
+# print(f'Nb of tests done: {test_case_id-1}')
+# print(f'Nb of tests failed: {nb_failed_tests}')
 
 
 print('---------------------------------------------------')
@@ -98,9 +106,11 @@ val = 0.00001
 increment = 0.00001
 nb_failed_tests = 0
 test_case_id = 1
+iterations = 99999
+format_str = "{:.5f}"
 
-while val < 1:
-    result = execute_round_to_four_decimals_with_decimal(val, float(str(val)[0:6]), 4, test_case_id)
+for _ in range(iterations):
+    result = execute_round_to_four_decimals_with_decimal(val, float(str(val)[0:6]), 4, test_case_id, format_str)
     if not result:
         nb_failed_tests += 1
 
